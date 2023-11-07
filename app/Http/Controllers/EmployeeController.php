a8d02ce84e27ee7bd4f94dcfbb56b298b586a40d
PremSundar19 <premkumar1919999@gmail.com>
2023-11-07 10:31:50 +0530

    m

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
-use Illuminate\validate\rule;
-use Illuminate\Validation\ValidationException;
 use DB;
 
 class EmployeeController extends Controller
